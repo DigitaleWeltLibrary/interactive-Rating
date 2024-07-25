26e00f62353968f669f8fb8655986f8853e8e7c1
Raphael Bröderbauer <info@digitaleweltlibrary.at>
2024-07-25 11:57:10 +0200

    my Footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,21 @@
+import React, { PureComponent } from "react";
+
+function Footer() {
+  return (
+    <footer class="my-footer-my-data">
+      <p>
+        Challenge by
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by
+        <a href="https://www.founder.digitaleweltlibrary.at/">
+          Raphael Bröderbauer
+        </a>
+        & <a href="https://www.digitaleweltlibrary.at/">DWL</a>.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
